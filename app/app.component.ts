4434bfd976808f29e7729249a2a3b53028792756
Jordan Meier and Matt Rosanio <mmrosani@syr.edu>
2016-03-22 09:38:56 -0700

    add a click event emitter to the html in the component, and a new method to the AppComponent class

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,9 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
+        {{ task.description }}
+      </h3>
     </div>
   `
 })
@@ -23,6 +25,9 @@ export class AppComponent {
       new Task("Rewatch all of the Lord of the Rings movies.", 2),
       new Task("Do the laundry.", 3)
     ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
   }
 }
 
